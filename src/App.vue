2a773d02ed16a42ee65cc1f08509e0f0b25553e6
pan <371583920@qq.com>
2019-09-10 16:10:50 +0800

    Project initialization

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,8 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
+  <div id="app">  
     <router-view/>
   </div>
 </template>
+<style lang="stylus">
 
-<script>
-export default {
-  name: 'App'
-}
-</script>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
---
+++ src/pages/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home">
+    <a href="">列表页</a>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+
+export default {
+  name: 'home',
+  components: {
+    
+  }
+}
+</script>
